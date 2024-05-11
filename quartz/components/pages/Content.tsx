75c9eac4287d9fd07f4c6c1a5c238c4b937fa278
icepro <icepro@xd.com>
2024-05-11 16:18:33 +0800

    feat: 在没有永久链接的时候不添加链接

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -16,7 +16,7 @@ const Content: QuartzComponent = ({ fileData, tree, cfg }: QuartzComponentProps)
     {content}
     {openCCBY && <div class="cc-by">
       <p><b>本文标题：</b>{fileData.frontmatter?.title}</p>
-      <p><b>本文链接：</b><a href={link}>{link}</a></p>
+      {permalink && <p><b>本文链接：</b><a href={link}>{link}</a></p>}
       <p><b>作者授权：</b>{isPageOriginal ? "本文由 icepro 原创编译并授权刊载发布。" : "本文为转载内容，详情见正文标注"}</p>
       {isPageOriginal && <p><b>版权声明：</b>本文使用「署名-非商业性使用-相同方式共享 4.0 国际」创作共享协议，转载或使用请遵守署名协议。</p>}
     </div>}
