c853c2506dbb36888e81fb208a3bb05654a1e092
icepro <icepro@xd.com>
2024-05-28 19:53:13 +0800

    feat: 支持评论区 KaTeX 自动渲染

--- quartz/components/scripts/comment.inline.ts
+++ quartz/components/scripts/comment.inline.ts
@@ -13,7 +13,11 @@ function loadScript(url: string) {
   });
 }
 async function init() {
-  await loadScript('https://cdnjs.cloudflare.com/ajax/libs/twikoo/1.6.36/twikoo.min.js');
+  await Promise.all([
+    loadScript('https://cdnjs.cloudflare.com/ajax/libs/twikoo/1.6.36/twikoo.min.js'),
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.js"),
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/contrib/auto-render.min.js"),
+  ]);
   (window as any).twikoo?.init({
     envId: 'https://comment.iceprosurface.com',
     el: '#tcomment',
