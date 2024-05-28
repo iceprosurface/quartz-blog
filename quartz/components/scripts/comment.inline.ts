3cd323dd08bc92f4ff057f30771b1be7e288f2c3
icepro <icepro@xd.com>
2024-05-28 19:55:45 +0800

    fix: 修复 comment 图片 margin 问题、修复auto render 以来 katex 的问题

--- quartz/components/scripts/comment.inline.ts
+++ quartz/components/scripts/comment.inline.ts
@@ -15,8 +15,8 @@ function loadScript(url: string) {
 async function init() {
   await Promise.all([
     loadScript('https://cdnjs.cloudflare.com/ajax/libs/twikoo/1.6.36/twikoo.min.js'),
-    loadScript("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.js"),
-    loadScript("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/contrib/auto-render.min.js"),
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/katex.min.js")
+      .then(() => loadScript("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.16.9/contrib/auto-render.min.js")),
   ]);
   (window as any).twikoo?.init({
     envId: 'https://comment.iceprosurface.com',
