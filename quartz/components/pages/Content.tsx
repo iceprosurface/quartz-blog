a6c4c451d21dfaa55e4f4dccf86e5a87c897a318
icepro <icepro@xd.com>
2024-05-29 11:45:42 +0800

    fix: 修复错误的 ccby 和 link 判断逻辑

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -16,11 +16,15 @@ const Content: QuartzComponent = ({ fileData, tree, cfg }: QuartzComponentProps)
   const isPageOriginal = Boolean(fileData.frontmatter?.isPageOriginal ?? true);
   return <article class={classString}>
     {content}
-    {openCCBY && <div class="cc-by">
+    {permalink && <div class="cc-by">
       <p><b>本文标题：</b>{fileData.frontmatter?.title}</p>
       {permalink && <p><b>本文链接：</b><a href={link}>{link}</a></p>}
-      <p><b>作者授权：</b>{isPageOriginal ? "本文由 icepro 原创编译并授权刊载发布。" : "本文为转载内容，详情见正文标注"}</p>
-      {isPageOriginal && <p><b>版权声明：</b>本文使用「署名-非商业性使用-相同方式共享 4.0 国际」创作共享协议，转载或使用请遵守署名协议。</p>}
+      {
+        openCCBY && (<p><b>作者授权：</b>{isPageOriginal ? "本文由 icepro 原创编译并授权刊载发布。" : "本文为转载内容，详情见正文标注"}</p>)
+      }
+      {
+        openCCBY && isPageOriginal && <p><b>版权声明：</b>本文使用「署名-非商业性使用-相同方式共享 4.0 国际」创作共享协议，转载或使用请遵守署名协议。</p>
+      }
     </div>}
     <script dangerouslySetInnerHTML={{ __html: `window.__comment_id__ = "${permalink}"` }}></script>
     {hasComments && permalink && <div id="tcomment"></div>}
