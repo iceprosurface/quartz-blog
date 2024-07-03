c4228361ffad53230190996d98a10eb317d57e9a
icepro <icepro@xd.com>
2024-07-03 13:32:47 +0800

    feat: style 添加 spa preserve

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -36,7 +36,7 @@ export default (() => {
             <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
           </>
         )}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lxgw-wenkai-webfont/1.7.0/style.css" />
+        <link rel="stylesheet" spa-preserve href="https://cdnjs.cloudflare.com/ajax/libs/lxgw-wenkai-webfont/1.7.0/style.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
