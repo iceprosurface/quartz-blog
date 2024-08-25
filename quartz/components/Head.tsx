47b2d00bfd567194ed5f5c3d4bae95118216d3b4
Aaron Pham <Aaronpham0103@gmail.com>
2024-08-25 03:33:14 -0400

    feat: 同步主线 graph 优化

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -26,7 +26,6 @@ export default (() => {
         <link rel="alternate" type="application/rss+xml" title="最近的 15 条" href="/index.xml"></link>
         <link rel="alternate" type="application/rss+xml" title="全部笔记" href="/rss-full.xml"></link>
         <meta name="excalidraw-plugin" spa-preserve data-js content="/static/quartz-excalidraw-plugin.js" />
-        <meta name="graph-plugin" spa-preserve data-js content="/static/quartz-graph-plugin.js" />
         <meta name="contentIndex" data-json content="/static/contentIndex.json" />
         {
           isPageReproduction && <meta name="robots" content="noindex" />
