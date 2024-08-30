f9556a888349a06f7c1f46955c688bbfb93eaab1
icepro <icepro@xd.com>
2024-08-30 11:49:43 +0800

    feat: 添加搜索的中文翻译

--- quartz/components/scripts/search.inline.ts
+++ quartz/components/scripts/search.inline.ts
@@ -338,8 +338,8 @@ document.addEventListener("nav", async (e: CustomEventMap["nav"]) => {
     removeAllChildren(results)
     if (finalResults.length === 0) {
       results.innerHTML = `<a class="result-card no-match">
-          <h3>No results.</h3>
-          <p>Try another search term?</p>
+          <h3>暂无数据</h3>
+          <p>试试其他搜索词?</p>
       </a>`
     } else {
       results.append(...finalResults.map(resultToHTML))
