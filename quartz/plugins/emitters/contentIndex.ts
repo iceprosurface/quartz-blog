f745b5d01d423e99879f9ced0c6c8ac9501aea42
icepro <icepro@xd.com>
2024-10-16 11:28:30 +0800

    feat: 调整认证字段位置

--- quartz/plugins/emitters/contentIndex.ts
+++ quartz/plugins/emitters/contentIndex.ts
@@ -89,13 +89,13 @@ function generateRSSFeed(cfg: GlobalConfiguration, idx: ContentIndex, limit?: nu
 
   return `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
-    <follow_challenge>
-        <feedId>52340201851637774</feedId>
-        <userId>68901958086173696</userId>
-    </follow_challenge>
     <channel>
       <title>${escapeHTML(cfg.pageTitle)}</title>
       <link>https://${base}</link>
+      <follow_challenge>
+        <feedId>52340201851637774</feedId>
+        <userId>68901958086173696</userId>
+      </follow_challenge>
       <description>${limit === Infinity ? `在 ${cfg.pageTitle} 上的 ${currentFull.length} 条笔记` : description}</description>
       <generator>Quartz -- quartz.jzhao.xyz</generator>
       ${items}
